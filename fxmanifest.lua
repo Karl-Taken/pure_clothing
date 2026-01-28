afdbe79e92c71c2b28bbc5f4b5881453c24e32d7
Karl Remo <karlangelremo@gmail.com>
2026-01-27 20:32:54 -0500

    Clothing Changelog
    Female:
    - Added Alycia Hair for female ped made by [jinxmods](https://www.gta5-mods.com/users/jinxmods) 25k poly
    - Added Anita Hair for female ped made by [jinxmods](https://www.gta5-mods.com/users/jinxmods) 24k poly
    - Added Wavy Hair for female ped made by [jinxmods](https://www.gta5-mods.com/users/jinxmods)
    - Added Short Simple Hair for female ped made by [jinxmods](https://www.gta5-mods.com/users/jinxmods) 14k poly
    - Added Ponytail with Strains for female ped made by [jinxmods](https://www.gta5-mods.com/users/jinxmods) <10k poly
    - Added Beatrice Hair for female ped made by [wanexia](https://www.gta5-mods.com/users/wanexia) 14k poly
    - Added Curly Ombre Hair for female ped made by [Jazlyn13](https://www.gta5-mods.com/users/Jazlyn13) <25k poly
    - Added Two long braids for female ped maded by [Jaagoda](https://www.gta5-mods.com/users/Jaagoda) 30k poly!
    
    - Added Halloween Nails for female ped made by [Frenchyluv](https://www.gta5-mods.com/users/Frenchyluv)
    - Added Felony Biker Vest for female ped by [clutit](https://www.gta5-mods.com/users/clutit)
    
    Male:
    - Added Mullet for male ped made by [SirDesse](https://www.gta5-mods.com/users/SirDesse) <15k poly
    - Added slickback for male ped by [Global Shop](https://www.gta5-mods.com/users/Global%20Shop) 33k poly!
    - Added 5 Beards for male peds by [clutit](https://www.gta5-mods.com/users/clutit) <5k poly each
    
    - Added Trucker Vest for male ped by [SirDesse](https://www.gta5-mods.com/users/SirDesse)
    - Added Redneck Pack for male ped by [SirDesse](https://www.gta5-mods.com/users/SirDesse)
    - Added Felony Biker Vest for male ped by [clutit](https://www.gta5-mods.com/users/clutit)

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
+
+fx_version 'cerulean'
+game       'gta5'
+
+author 'Karl Remo'
+description 'Pure Roleplay Clothing Pack'
+version '1.0.0'
+
+files {
+  'data/*.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp_m_sasp_uniform_shop.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp_m_hair_female_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_mp_f_hair_female_shop.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp_m_hair_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_mp_f_hair_shop.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp25_redneckpack.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp_m_pure_clothings_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_mp_f_pure_clothings_shop.meta'
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_m_freemode_01_mp_m_pure_medical_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'data/mp_f_freemode_01_mp_f_pure_medical_shop.meta'
+
+data_file 'ALTERNATE_VARIATIONS_FILE' 'data/pedalternatevariations.meta'
